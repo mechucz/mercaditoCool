1b076aaaa98ef0d8c56d008f700e5231d78d19cc
mechucz <mechucz@gmail.com>
2022-07-13 16:54:55 -0400

    SASS2+SEO

--- pages/mail.php
+++ pages/mail.php
@@ -7,7 +7,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 <link rel="stylesheet" href="../assets/scss/main.css">
 	<link rel="shortcut icon" href="../assets/images/favicon.ico" type="image/x-icon">
-<title>Contacto</title>
+<title>Contacto Mercadito Cool</title>
 </head>
 
 <body class="novedades">
